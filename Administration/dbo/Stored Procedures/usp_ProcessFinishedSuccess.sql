b099935ef3478a49a6bd5b433598910c34ac73fe
Lukasz27 <lukasz.bielak1@gmail.com>
2019-11-03 14:04:43 +0100

    Added new feature - check last processing status from ADF

---
+++ Administration/dbo/Stored Procedures/usp_ProcessFinishedSuccess.sql
@@ -0,0 +1,15 @@
+﻿--Procedure for chceck last status of partition manager execution
+--set model id which processing status should be verified
+create procedure dbo.usp_ProcessFinishedSuccess @ModelID INT
+as
+declare @maxId int
+declare @lastStatus char(6)   
+set @maxId = (select max([PartitioningLogID]) from [dbo].[ProcessingLog]) 
+set @lastStatus = (select LEFT([Message],6) from [dbo].[ProcessingLog] where [ModelConfigurationID] = @ModelID and [PartitioningLogID] = @maxId)
+
+if @lastStatus = 'Finish'
+	select 1
+else 
+	raiserror(50005,10,1,N'Failed')
+	
+
